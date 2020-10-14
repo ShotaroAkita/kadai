ec59d14fa9f7415ad7eb6dc90a099702726dc81a
ShotaroAkita <otogame010nasi@gmail.com>
2020-10-14 05:28:59 +0000

    課題02

---
+++ kadai-02/index.js
@@ -0,0 +1,17 @@
+const firstName = 'Shotaro' //名前
+const familyName = 'Akita' //名字
+console.log(firstName + familyName) //姓名を合わせて出力
+
+// 1 から10までの数字の和を表示する
+console.log(1 + 2 + 3 + 4 +5+6+7+8+9+10)
+
+//真偽型の演算
+const dog = false
+const cat = false
+//犬または猫を飼っている
+console.log(dog || cat)
+//犬と猫の両方を飼っている
+console.log(dog && cat)
+//犬も猫も飼ってない
+console.log(!dog && !cat)
+console.log(!(dog || cat))  //別回答
