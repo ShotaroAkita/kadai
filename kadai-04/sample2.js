44b9e2db9f642d0c86841d5267529f27edd2969d
ShotaroAkita <otogame010nasi@gmail.com>
2021-01-02 03:37:51 +0000

    Fix

---
+++ kadai-04/sample2.js
@@ -0,0 +1,8 @@
+function add(n1, n2, n3){
+    return(n1 + n2 + n3)
+    console.log('ここはじっこうされない')
+}
+
+const result1 = add(10, 100, 5)
+
+console.log(result1)
---
+++ kadai-04/sample3.js
@@ -0,0 +1,15 @@
+//関数は、関数型という値の一種
+function theFunction(){
+    console.log('Hello')
+}
+
+console.log(typeof theFunction)
+const myFunction = theFunction
+
+theFunction()
+myFunction()
+
+const myFunction2 = function(){
+    console.log('hello2')
+}
+myFunction2()
